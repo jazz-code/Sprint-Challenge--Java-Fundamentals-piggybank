6c8fa6f9f4b61e654ae2ae9fed05d97c38544f6c
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-06 11:46:37 -0400

    Coin changed to abstract class because all classes are similar

--- piggybank/src/piggybank/Coin.java
+++ piggybank/src/piggybank/Coin.java
@@ -1,8 +1,28 @@
 package piggybank;
 
-public interface coin
+public abstract class Coin
 {
-  void value();
-  void added();
-  void totalValue();  
+    int value;
+    double total;
+
+    public Coin(int value, double total)
+    {
+        this.value = value;
+        this.total = total;
+    }
+    //default contructor set to 1
+    public Coin()
+    {
+        value = 1;
+    }
+    //add value
+    public void added()
+    {
+        value = value + value;
+    }
+    // return the value * total
+    public int totalValue()
+    {
+        return value * total;
+    }
 }
---
+++ piggybank/src/piggybank/Dollar.java
@@ -0,0 +1,14 @@
+package piggybank;
+
+public class Dollar extends Coin
+{
+    private int value = 100;
+    
+    //constructor default super()
+    public Dollar()
+    {
+
+    }
+
+   
+}
